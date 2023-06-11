eba16a9359e6c1c5985f9ebec19fe1ad675cc7c7
Leandro_Tonello <leandronicolas1177@gmail.com>
2023-06-11 19:15:00 -0300

    Script funcionando con Interfaz

--- CallManipulate/CallManipulate.py
+++ CallManipulate/CallManipulate.py
@@ -1,4 +1,15 @@
 import re
+import openpyxl
+import pandas as pd
+import datetime
+import random
+import string
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Font, Border, Side
+from tkinter import ttk
+
 
 # Definicion de la clase Persona
 class Persona:
@@ -7,141 +18,166 @@ class Persona:
         self.nombre = nombre
         self.telefono = telefono
         self.documento = documento
-        self.tarjetas = tarjetas
+        self.tarjetas = self.etiquetar_tarjetas(tarjetas)
 
-texto = """
-1
- Nombre   : GIOFFRE, LUCAS DANIEL                    Doc.    : DU    36.901.296
- Act.Ppal : OTRAS ACTIVIDADES                                                  
- Direccion: AIZPURUA BENITO 2575 2 40                Fe.Alta : 22-06-2017      
-            (C1431ERC) CIUDAD AUTONOMA BUENOS AI     Fe.Nac. : 03-10-1993      
-            CAPITAL FEDERAL, ARGENTINA               Cod.Post.: (1431)         
- Telefono : (11)>3163-5365 Int.:              Tlx.:         Fax:               
- . Tipo            Mo Cuenta                Fecha  Saldo Deudor  Saldo Acreed. 
- . VISA ORO           4546420024951918                                         
-
-2
- Nombre   : GIORGIUTTI, MARIA CECILIA                Doc.    : DU    17.713.125
- Act.Ppal : HOTELES Y/O RESTAURANTES                                           
- Direccion: MONSENOR LARUMBE 1115                    Fe.Alta : 02-08-2004      
-            (1640) MARTINEZ                          Fe.Nac. : 12-04-1966      
-            BUENOS AIRES, ARGENTINA                                            
- Telefono : (11)>4144-8799 Int.:              Tlx.:         Fax:               
- . Tipo            Mo Cuenta                Fecha  Saldo Deudor  Saldo Acreed. 
- . TODO SUMA          0001732753                                               
- . ELECTRON                                                                    
- . FILTRAR            020953388                                                
- . CAJA DE AHORRO   $ 4004362-1 142-1      ACTUAL                      9.222,52
- . DEB.AUTOMATICO                                                              
- . OPERACION DE ME                                                             
- . NOSIS              004034773                                                
- . MENSAJES           0003461779                                               
- . SERV. PREFER                                                                
- . -VISA PLATINUM     4338300009675678                                         
- . -MASTER PLA        5239372783200707                                         
-
-3
- Nombre   : GIOVANELLI, JUAN                         Doc.    : DU    33.079.910
- Act.Ppal : SERVICIOS PARA EMPRESAS                                            
- Direccion: DON BOSCO 1499 3 25 B                    Fe.Alta : 25-08-2014      
-            (B1642FEE) SAN ISIDRO                    Fe.Nac. : 26-06-1987      
-            BUENOS AIRES, ARGENTINA                  Cod.Post.: (1642)         
- Telefono : (11)>6885-3093 Int.:              Tlx.:         Fax:               
- . Tipo            Mo Cuenta                Fecha  Saldo Deudor  Saldo Acreed. 
- . VISA BUSINESS      4937028000412826                                         
- . EMINENT DEBITO                                                              
- . RADOC            $  803974-3 209-7      NODISP                              
- . FILTRAR            033902570                                                
- . VERAZ EXP.         035720795                                                
- . OPERACION DE ME                                                             
- . MENSAJES           0009542785                                               
- . SV.EMINENT CONV                                                             
- . -EMP.ASOCIADA                                                               
- . -VISA SIGNATURE    4593540005400551                                         
- . -MASTER BLK        5505684430500546                                         
-
-4
- Nombre   : GIRALT, ESTEBAN ADOLFO ANDRES            Doc.    : DU    24.529.714
- Act.Ppal : SERVICIOS DE ASESORIA                                              
- Direccion: MEXICO 1036                              Fe.Alta : 19-09-2007      
-            (B1640DLF) MARTINEZ                      Fe.Nac. : 01-01-1975      
-            BUENOS AIRES, ARGENTINA                  Cod.Post.: (1640)         
- Telefono : (11)>5327-4985 Int.:              Tlx.:         Fax:               
- . Tipo            Mo Cuenta                Fecha  Saldo Deudor  Saldo Acreed. 
- . ELECTRON                                                                    
- . FILTRAR            033208719                                                
- . SOLICITUD ADIC     00000020747699                                           
- . VERAZ EXP.         035898241                                                
- . OPERACION DE ME                                                             
- . MENSAJES           0011431491                                               
- . SERV. PREFER                                                                
- . -VISA ORO          4546420029162289                                         
- . -MASTER.ORO        5456524205615567                                         
-
-5
- Nombre   : GIRALT, MARIA MERCEDES                   Doc.    : DU    30.495.380
- Act.Ppal : ENSENANZA                                                          
- Direccion: AMEGHINO FLORENTINO 355                  Fe.Alta : 21-09-2007      
-            (B1643DTE) BECCAR                        Fe.Nac. : 14-09-1983      
-            BUENOS AIRES, ARGENTINA                  Cod.Post.: (1643)         
- Telefono : (11)>3588-0367 Int.:              Tlx.:         Fax:               
- . Tipo            Mo Cuenta                Fecha  Saldo Deudor  Saldo Acreed. 
- . MASTER.ORO         5456520317547789                                         
-
-6
-  Nombre   : GIRARDI, MARIA EMILIA                    Doc.    : DU    31.722.224
-  Act.Ppal : OTRAS ACTIVIDADES                                                  
-  Direccion: ELCANO SEBASTIAN 490                     Fe.Alta : 10-09-2008      
-             (B1641AYB) ACASSUSO                      Fe.Nac. : 18-07-1985      
-             BUENOS AIRES, ARGENTINA                  Cod.Post.: (1640)         
-  Telefono : (11)>5616-5024 Int.:              Tlx.:         Fax:               
-  . Tipo            Mo Cuenta                Fecha  Saldo Deudor  Saldo Acreed. 
-  . VISA PLATINUM      4338300007752834                                         
-  . VISA INTERNAC.     4546400067353818                                         
-  . VISA ORO           4546420004919018                                         
-  . AMEX PLATINUM      377797004554552                                          
-
-7
-  Nombre   : GIUNTI, LARA MARIEL                      Doc.    : DU    36.914.785
-  Act.Ppal : OTRAS ACTIVIDADES                                                  
-  Direccion: AVENIDA MARIA 1155                       Fe.Alta : 04-05-2015      
-             (1611) DON TORCUATO                      Fe.Nac. : 21-10-1992      
-             BUENOS AIRES, ARGENTINA                                            
-  Telefono : (11)>5991-6240 Int.:              Tlx.:         Fax:               
-  . Tipo            Mo Cuenta                Fecha  Saldo Deudor  Saldo Acreed. 
-  . VISA INTERNAC.     4546400074989125                                         
-  . VISA ORO           4546420028099508                                         
-  . AMEX ORO           377796011953864                                          
-                                      
-"""
-
-# Patrones de expresiones regulares
-patron_persona = r"\n(\d+)\n\s+Nombre\s+:\s+([^:\n]+)\s+Doc\.\s+:\s+DU\s+([^:\n]+)([\s\S]+?)(?=\n\d+\n|\Z)"
-patron_telefono = r"Telefono\s+:\s+([^:\n]+)"
-patron_tarjeta = r"\b\d{15,16}\b"
-
-# Extraccion de informacion de todas las personas
-personas = []
-matches_personas = re.finditer(patron_persona, texto, re.MULTILINE)
-for match_persona in matches_personas:
-    numero_cliente = match_persona.group(1).strip()
-    nombre = match_persona.group(2).strip()
-    documento = match_persona.group(3).strip()
-    bloque_persona = match_persona.group(4)
-
-    telefono = re.search(patron_telefono, bloque_persona).group(1).strip().replace(" Int.", "")
-    tarjetas = re.findall(patron_tarjeta, bloque_persona)
-
-    persona = Persona(numero_cliente, nombre, telefono, documento, tarjetas)
-    personas.append(persona)
+    def etiquetar_tarjetas(self, tarjetas):
+        etiquetas = []
+        for tarjeta in tarjetas:
+            if tarjeta.startswith("4"):
+                etiquetas.append("Visa: " + tarjeta)
+            elif tarjeta.startswith("5"):
+                etiquetas.append("Master: " + tarjeta)
+            elif tarjeta.startswith("3"):
+                etiquetas.append("Amex: " + tarjeta)
+            else:
+                etiquetas.append(tarjeta)
+        return etiquetas
 
 
-# Mostrar informacion por pantalla para cada persona
-for persona in personas:
-    print("Numero de cliente:", persona.numero_cliente)
-    print("Nombre:", persona.nombre)
-    print("Telefono:", persona.telefono)
-    print("Documento:", persona.documento)
-    for i, tarjeta in enumerate(persona.tarjetas, start=1):
-        print("Tarjeta", i, ":", tarjeta)
-    print()  # Separador entre personas
+def generar_archivo():
+    try:
+        # Obtener la ubicacion del archivo de entrada
+        ruta_archivo = entrada_archivo.get()
+
+        if not ruta_archivo:
+            raise ValueError("No se selecciono un archivo de entrada.")
+
+        # Busqueda de la info en el Excel
+        datos = pd.read_excel(ruta_archivo, usecols=[0], engine="openpyxl")
+        columna_0 = datos.iloc[:, 0]  # Acceder a la columna 0 del DataFrame
+        texto = ""
+        for valor in columna_0:
+            texto += str(valor) + "\n"
+
+        # Patrones de expresiones regulares
+        patron_persona = r"\n(\d+)\n\s+Nombre\s+:\s+([^:\n]+)\s+Doc\.\s+:\s+DU\s+([^:\n]+)([\s\S]+?)(?=\n\d+\n|\Z)"
+        patron_telefono = r"Telefono\s+:\s+([^:\n]+)"
+        patron_tarjeta = r"\b\d{15,16}\b"
+
+        # Extraccion de informacion de todas las personas
+        personas = []
+        matches_personas = re.finditer(patron_persona, texto, re.MULTILINE)
+        for match_persona in matches_personas:
+            numero_cliente = match_persona.group(1).strip()
+            nombre = match_persona.group(2).strip()
+            documento = match_persona.group(3).strip()
+            bloque_persona = match_persona.group(4)
+
+            telefono = re.search(patron_telefono, bloque_persona).group(1).strip().replace(" Int.", "").replace(">", "").replace("Int.", "")
+            tarjetas = re.findall(patron_tarjeta, bloque_persona)
+
+            persona = Persona(numero_cliente, nombre, telefono, documento, tarjetas)
+            personas.append(persona)
+
+        # Crear un libro y una hoja de calculo con openpyxl
+        workbook = Workbook()
+        worksheet = workbook.active
+
+        # Crear el DataFrame
+        data = []
+        for persona in personas:
+            tarjetas = ', '.join(persona.tarjetas)
+            data.append([persona.numero_cliente, persona.nombre, persona.telefono, tarjetas])
+
+        df = pd.DataFrame(data, columns=['Nro', 'Nombre', 'Telefono', 'Tarjetas'])
+
+        # Escribir los datos en la hoja de calculo
+        for row_num, row_data in enumerate(df.values, 2):
+            for col_num, value in enumerate(row_data, 1):
+                cell = worksheet.cell(row=row_num, column=col_num, value=value)
+
+        # Configurar el formato y estilo
+        header_fill = PatternFill(fill_type='solid', fgColor='000000')
+        header_font = Font(color='FFFFFF', bold=True)
+        data_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
+        gray_fill = PatternFill(fill_type='solid', fgColor='E9E9E9')
+
+        # Aplicar formato y estilo a los encabezados
+        for col_num, header in enumerate(df.columns, 1):
+            cell = worksheet.cell(row=1, column=col_num, value=header)
+            cell.fill = header_fill
+            cell.font = header_font
+
+        # Aplicar formato y estilo a las celdas de datos
+        for row in worksheet.iter_rows(min_row=2, min_col=1, max_col=df.shape[1]):
+            for cell in row:
+                cell.border = data_border
+
+        # Aplicar formato y estilo a las filas alternas
+        for row_num in range(2, worksheet.max_row + 1, 2):
+            for col_num in range(1, worksheet.max_column + 1):
+                cell = worksheet.cell(row=row_num, column=col_num)
+                cell.fill = gray_fill
+
+        # Ajustar el ancho de las columnas
+        for column in worksheet.columns:
+            max_length = max(len(str(cell.value)) for cell in column)
+            adjusted_width = (max_length + 2) * 1.2
+            worksheet.column_dimensions[column[0].column_letter].width = adjusted_width
+
+        # Generar un nombre de archivo aleatorio basado en la fecha y hora
+        timestamp = datetime.datetime.now().strftime("%Y%m%d")
+        random_word = ''.join(random.choices(string.ascii_lowercase, k=5))
+        nombre_archivo = f"{timestamp}_{random_word}.xlsx"
+
+        # Ruta de guardado con el nombre de archivo generado
+        ruta_guardado = filedialog.asksaveasfilename(defaultextension=".xlsx")
+
+        if not ruta_guardado:
+            raise ValueError("No se selecciono una ubicacion de guardado.")
+
+        # Guardar el archivo Excel
+        workbook.save(ruta_guardado)
+        workbook.close()
+
+        # Mostrar la ubicacion del archivo generado
+        resultado_archivo.config(text="Archivo generado:\n" + ruta_guardado)
+        messagebox.showinfo("Proceso completado", "El archivo se ha generado correctamente.")
+
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+# Función para seleccionar un archivo
+def seleccionar_archivo():
+    archivo = filedialog.askopenfilename(filetypes=[("Archivos Excel", "*.xlsx")])
+    entrada_archivo.delete(0, tk.END)
+    entrada_archivo.insert(0, archivo)
+
+# Crear la interfaz de usuario
+window = tk.Tk()
+window.title("Generador de Archivo Excel")
+
+style = ttk.Style()
+style.configure("TButton", font=("Arial", 12), padding=6)
+
+# Etiqueta para mostrar el texto "Seleccione el Excel"
+lbl_archivo = ttk.Label(window, text="Seleccione el Excel:", font=("Arial", 12, "bold"))
+lbl_archivo.pack(pady=10)
+
+# Marco que contiene el campo de entrada y el botón "Seleccionar"
+frame_seleccionar = ttk.Frame(window)
+frame_seleccionar.pack(pady=5)
+
+# Campo de entrada deshabilitado para mostrar la ubicación del archivo seleccionado
+entrada_archivo = ttk.Entry(frame_seleccionar, width=50, font=("Arial", 12), state="enabled")
+entrada_archivo.pack(side=tk.LEFT, padx=5)
+
+# Botón para seleccionar el archivo
+btn_seleccionar = ttk.Button(frame_seleccionar, text="Seleccionar", command=seleccionar_archivo)
+btn_seleccionar.pack(side=tk.LEFT)
+
+# Botón de ejecutar
+btn_ejecutar = ttk.Button(window, text="Ejecutar", command=generar_archivo)
+btn_ejecutar.pack(pady=10)
+
+# Etiqueta para mostrar la ubicación del archivo generado
+resultado_archivo = ttk.Label(window, text="")
+resultado_archivo.pack()
+
+# Calcula el tamaño de la ventana teniendo en cuenta el espacio requerido por los elementos internos
+window.update_idletasks()
+width = max(window.winfo_reqwidth(), 800) + 10
+height = max(window.winfo_reqheight(), 200) + 10
+window.geometry(f"{width}x{height}")
+
+window.mainloop()
